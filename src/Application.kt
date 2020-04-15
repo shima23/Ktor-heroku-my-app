904f8168106097ab9ec834343e4da8803b623ac7
dhayashi <dhayashi@sonix.asia>
2020-04-15 15:51:35 +0900

    insert

--- src/Application.kt
+++ src/Application.kt
@@ -2,11 +2,18 @@ package com.myapp
 
 import com.myapp.common.DBInitializer
 import com.myapp.common.EnvConstants
+import com.myapp.model.User
+import com.myapp.model.UserData
+import com.myapp.model.Users
+import com.myapp.model.toData
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -16,7 +23,16 @@ fun Application.module(testing: Boolean = false) {
     DBInitializer().execute()
     routing {
         get("/" ) {
-            call.respond("Hello World")
+            lateinit var user :UserData
+            transaction {
+                user = User.new {
+                    name = "Tom"
+                    age = 20
+                    createdAt = DateTime.now()
+                    updatedAt = DateTime.now()
+                }.toData()
+            }
+            call.respond("Hello World : " + user.name)
         }
     }
 }
